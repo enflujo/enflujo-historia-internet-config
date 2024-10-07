95e7b11bbe25c709643f820fc3417d60cca51a86
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-10-07 15:50:33 -0500

    🎉 Nueva colección glosario

--- colecciones.php
+++ colecciones.php
@@ -29,7 +29,7 @@ function historia_coleccion_personajes()
   return array(
     'labels' => array(
       'name' => __('Personajes', 'enflujo'),
-      'singular_name' => __('Personajes', 'enflujo'),
+      'singular_name' => __('Personaje', 'enflujo'),
       'add_new' => __(text: 'Añadir personaje', domain: 'enflujo'),
       'edit_item' => __(text: 'Editar personaje', domain: 'enflujo')
     ),
@@ -41,6 +41,27 @@ function historia_coleccion_personajes()
     'rewrite' => array('slug' => 'personajes'),
     'graphql_single_name' => 'personaje',
     'graphql_plural_name' => 'personajes',
+    'publicly_queryable' => true
+  );
+}
+
+function historia_coleccion_glosario()
+{
+  return array(
+    'labels' => array(
+      'name' => __('Glosario', 'enflujo'),
+      'singular_name' => __('Término', 'enflujo'),
+      'add_new' => __(text: 'Añadir término', domain: 'enflujo'),
+      'edit_item' => __(text: 'Editar término', domain: 'enflujo')
+    ),
+    'public' => true,
+    'has_archive' => false,
+    'menu_position' => 4,
+    'menu_icon' => 'dashicons-book',
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'rewrite' => array('slug' => 'glosario'),
+    'graphql_single_name' => 'termino',
+    'graphql_plural_name' => 'glosario',
     'publicly_queryable' => true
   );
 }
